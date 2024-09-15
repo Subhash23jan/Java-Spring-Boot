ebe788a2d2c2cc84c54d252857f915010df26b09
Subhash <Subhash613d@gmail.com>
2024-09-15 12:29:25 +0530

    first commit

---
+++ group-chat/group-chat/src/main/java/com/example/group_chat/GroupChatApplication.java
@@ -0,0 +1,15 @@
+package com.example.group_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupChatApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GroupChatApplication.class, args);
+
+	}
+
+}
